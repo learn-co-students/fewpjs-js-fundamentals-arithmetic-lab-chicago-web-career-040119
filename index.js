ed14d4f0ed65d410f2fd445a2593c36f0c1bed30
Jackson Robbins <jackrobbins1@gmail.com>
2019-05-21 13:37:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 9 % 5
+
+let numbers = [1, 2, 3, 20]
+
+// for (i = 0; i <= 20; i++) {
+//   numbers.push(i)
+// }
+
+let max = Math.max(...numbers)
