b8ab15962e22c475faa7df5724b1fed4521c76f3
NMoore42 <nicholas.moore5@icloud.com>
2019-05-21 12:54:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+let mod = 29 % 5;
+
+let max = Math.max(0, 4, 20)
